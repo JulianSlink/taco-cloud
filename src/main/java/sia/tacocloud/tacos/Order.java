ff0333f79ac05c05335884217e5433f8bc2aa733
22898 <2289860941@qq.com>
2023-01-01 16:30:55 +0800

    Chapter4 Spring Security

--- src/main/java/sia/tacocloud/tacos/Order.java
+++ src/main/java/sia/tacocloud/tacos/Order.java
@@ -50,6 +50,9 @@ public class Order {
     @ManyToMany(targetEntity = Taco.class)
     List<Taco> tacos = new ArrayList<>();
 
+    @ManyToOne
+    private User user;
+
     /**
      * @param saved
      */
--- src/main/java/sia/tacocloud/tacos/web/OrderController.java
+++ src/main/java/sia/tacocloud/tacos/web/OrderController.java
@@ -1,5 +1,6 @@
 package sia.tacocloud.tacos.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import sia.tacocloud.tacos.Order;
+import sia.tacocloud.tacos.User;
 import sia.tacocloud.tacos.data.OrderRepository;
 
 import javax.validation.Valid;
@@ -31,11 +33,13 @@ public class OrderController {
 
     @PostMapping
     public String processOrder(@Valid Order order, Errors errors,
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
 
+        order.setUser(user);
         orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
